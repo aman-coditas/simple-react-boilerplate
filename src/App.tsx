1060cf40253f6d72df89cabd1686e95a29819b35
Aman Kumar <aman.kumar@coditas.com>
2022-03-21 15:29:42 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles/styles.scss";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="wrapper" >
+                <h1>React 17 and TypeScript 4 App!</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
